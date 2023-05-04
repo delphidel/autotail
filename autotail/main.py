bd52fd6cb7e66bac5c308010e90d1c4c393a451b
delphidel <delphi.deluvian@protonmail.com>
2023-05-04 14:15:26 -0600

    posts a request, gets a 400... presumably because of recaptcha?

--- autotail/main.py
+++ autotail/main.py
@@ -1,19 +1,44 @@
 import requests
 import json
-
-url = 'https://api.guerrillamail.com/ajax.php'
-op = 'get_email_address'
-ip = '127.0.0.1'
-agent = 'does_not_matter'
-
-full_path = f'{url}?f={op}&ip={ip}&agent={agent}'
+from pathlib import Path
 
 
 def main():
-    print(f"Getting new address from {full_path}...")
+    story = (
+        'Fuck off, scabs. I hope almost every "story" you get '
+        'is from a Workers United ally.'
+    )
+    js = edit_template('rebecca.portia66@gmail.com', story)
+
+    resp = post_story(js)
+
+    print(resp)
+    print(resp.text)
+
+
+# TODO: rewrite to generate an address @gmail.com
+def get_email(url='https://api.guerrillamail.com/ajax.php',
+              ip='127.0.0.1',
+              user_agent='does_not_matter'):
+    full_path = f'{url}?f=get_email_address&ip={ip}&agent={user_agent}'
+
     resp = requests.get(full_path)
     js = json.loads(resp.text)
+    return js['email_addr']
 
-    new_email = js['email_addr']
-    print(new_email)
 
+def edit_template(email,
+                  story,
+                  path=Path(__file__).parent / 'constants/template.json'):
+    with open(path) as f:
+        js = json.load(f)
+        print(js['fields'])
+        js['fields']['3']['value'] = story
+        js['fields']['10']['value'] = email
+        return js
+        
+
+def post_story(js,
+               url='https://workersunitedfacts.com/wp/wp-admin/admin-ajax.php'):
+    return requests.post(url, json=js)
+    

621b2f5f4e063c6bc8b2c99706816392e3fe2e2f
delphidel <delphi.deluvian@protonmail.com>
2023-05-04 14:28:25 -0600

    Create a fake email + formatting

--- autotail/main.py
+++ autotail/main.py
@@ -1,44 +1,49 @@
 import requests
 import json
 from pathlib import Path
+from faker import Faker
+import random
 
 
 def main():
     story = (
         'Fuck off, scabs. I hope almost every "story" you get '
-        'is from a Workers United ally.'
+        "is from a Workers United ally."
     )
-    js = edit_template('rebecca.portia66@gmail.com', story)
+    email = make_email()
 
+    print(email)
+    print(story)
+
+    js = edit_template(make_email(), story)
     resp = post_story(js)
 
     print(resp)
     print(resp.text)
 
 
-# TODO: rewrite to generate an address @gmail.com
-def get_email(url='https://api.guerrillamail.com/ajax.php',
-              ip='127.0.0.1',
-              user_agent='does_not_matter'):
-    full_path = f'{url}?f=get_email_address&ip={ip}&agent={user_agent}'
+def make_email():
+    faker = Faker()
+    (first, last) = (faker.first_name(), faker.last_name())
+    if random.randrange(0, 2) == 0:
+        year = f"{random.randrange(0, 10)}{random.randrange(0, 10)}"
+    else:
+        decade = random.choice(["19", "20"])
+        if decade == "19":
+            year = f"{decade}{random.randrange(0, 10)}{random.randrange(0, 10)}"
+        else:
+            year = f"{decade}{random.randrange(0, 2)}{random.randrange(0, 10)}"
+    sep = random.choice([".", "_", "-"])
+    return f"{first}{sep}{last}{year}"
 
-    resp = requests.get(full_path)
-    js = json.loads(resp.text)
-    return js['email_addr']
 
-
-def edit_template(email,
-                  story,
-                  path=Path(__file__).parent / 'constants/template.json'):
+def edit_template(email, story, path=Path(__file__).parent / "constants/template.json"):
     with open(path) as f:
         js = json.load(f)
-        print(js['fields'])
-        js['fields']['3']['value'] = story
-        js['fields']['10']['value'] = email
+        js["fields"]["3"]["value"] = story
+        js["fields"]["10"]["value"] = email
         return js
-        
 
-def post_story(js,
-               url='https://workersunitedfacts.com/wp/wp-admin/admin-ajax.php'):
+
+def post_story(js, url="https://workersunitedfacts.com/wp/wp-admin/admin-ajax.php"):
     return requests.post(url, json=js)
-    
